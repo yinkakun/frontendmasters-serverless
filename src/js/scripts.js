0294399f005183ec53b164e95811689e881232fc
Yinka Adedire <yinkakun@gmail.com>
2021-11-23 15:29:14 +0100

    feat: fetch movies ratings from omdbapi

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,21 @@
+const addRatings = (element, ratings) => {
+  ratings.forEach((rating) => {
+    const item = document.createElement('li');
+    const label = document.createElement('span');
+    label.classList.add('source');
+    label.textContent =
+      rating.Source === 'Internet Movie Database' ? 'IMDB' : rating.Source;
+    item.appendChild(label);
+
+    const ratingValue = document.createElement('span');
+    ratingValue.textContent = rating.Value;
+    item.appendChild(ratingValue);
+
+    const ratingsElement = element.querySelector('.scores');
+    ratingsElement.appendChild(item);
+  });
+};
+
 async function initialize() {
   const movies = await fetch('/.netlify/functions/movies').then((res) =>
     res.json()
@@ -15,6 +33,8 @@ async function initialize() {
 
     element.querySelector('h2').textContent = movie.title;
     element.querySelector('.tagline').textContent = movie.title;
+
+    addRatings(element, movie.ratings);
 
     container.appendChild(element);
   });
